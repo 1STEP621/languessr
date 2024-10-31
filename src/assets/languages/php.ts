7ca06c926d989ec326f110591d37ba390d3733f0
結葉みれい <me@mirrei.dev>
2024-10-31 20:42:13 +0900

    fix: 改行コードが正しく表示されない問題を修正

--- src/assets/languages/php.ts
+++ src/assets/languages/php.ts
@@ -12,13 +12,13 @@ echo "Hello, world!";
         `<?php
 for ($i = 1; $i <= 100; $i++) {
     if ($i % 3 == 0 && $i % 5 == 0) {
-        echo "FizzBuzz\n";
+        echo "FizzBuzz\\n";
     } elseif ($i % 3 == 0) {
-        echo "Fizz\n";
+        echo "Fizz\\n";
     } elseif ($i % 5 == 0) {
-        echo "Buzz\n";
+        echo "Buzz\\n";
     } else {
-        echo $i . "\n";
+        echo $i . "\\n";
     }
 }
 ?>`
