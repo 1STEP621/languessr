feb5b87deb71f5faaacdc26c4df35e6ff6a4a936
1Step621 <onestep621@gmail.com>
2024-11-01 09:22:47 +0900

    feat: タイトルビュー

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import GameView from '@/assets/views/GameView.vue';
 import DisconnectedView from '@/assets/views/DisconnectedView.vue';
 import { useViewStore } from './stores/view';
+import TitleView from './assets/views/TitleView.vue';
 
 const view = useViewStore();
 </script>
 
 <template>
+  <TitleView v-if="view.state === 'title'" />
   <GameView v-if="view.state === 'game'" />
   <DisconnectedView v-if="view.state === 'disconnected'" />
 </template>
---
+++ src/assets/views/TitleView.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import Button from '../components/Button.vue';
+import BaseView from './BaseView.vue';
+import { useViewStore } from '../../stores/view';
+
+const view = useViewStore();
+</script>
+
+<template>
+  <BaseView :class="$style.view">
+    <div :class="$style.titleGroup">
+      <img :class="$style.icon" src="/icon.svg" alt="logo" />
+      <h1 :class="$style.title">Languessr</h1>
+    </div>
+    <Button :class="$style.start" @click="view.state = 'game'">スタート</Button>
+  </BaseView>
+</template>
+
+<style module>
+.view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 1.2em;
+
+  .titleGroup {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    .icon {
+      width: 20vh;
+    }
+
+    .title {
+      font-weight: bold;
+      font-size: 5em;
+    }
+  }
+
+  .start {
+    margin-block: 2em;
+    font-size: 1em;
+    padding: 1em 3em;
+    border-radius: 1em;
+  }
+}
+</style>
