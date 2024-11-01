7434a3d7ff925a750f87aeb88fe6694bccfeb7be
結葉みれい <me@mirrei.dev>
2024-11-01 18:17:39 +0900

    enhance: 難易度調整UIを作成

---
+++ src/assets/components/Difficulty.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+defineProps<{
+  name: string;
+  color: string;
+}>();
+</script>
+
+<template>
+  <div :class="$style.difficulty" :style="{ backgroundColor: color }">
+    <h2>{{ name }}</h2>
+    <div :class="$style.description">
+      <ul>
+        <slot />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style module>
+.difficulty {
+  color: #ffffff;
+
+  border-radius: 1.5em;
+  padding: 1.5em;
+
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60vw;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 0px;
+    background: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 5 5"><path d="m0 4 4-4h1v1L1 5H0zM0 0h1L0 1M5 5V4L4 5"/></svg>');
+    filter: invert(1);
+    z-index: 0;
+    opacity: 0.05;
+  }
+
+  h2 {
+    font-size: 1.2em;
+    font-weight: bold;
+  }
+
+  &:global(.v-enter-active),
+  &:global(.v-leave-active) {
+    transition: opacity 200ms ease;
+  }
+  &:global(.v-enter-from),
+  &:global(.v-leave-to) {
+    opacity: 0;
+  }
+}
+.description {
+  background-color: #ffffff20;
+  color: #ffffff;
+
+  border-radius: 1em;
+  padding: 1em 1.5em;
+
+  text-align: left;
+
+  ul {
+    padding-inline-start: 1em;
+    padding-block: 0px;
+  }
+}
+</style>
--- src/assets/views/TitleView.vue
+++ src/assets/views/TitleView.vue
@@ -1,18 +1,39 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useGameStore } from '@/stores/game';
 import Button from '../components/Button.vue';
 import BaseView from './BaseView.vue';
 import { useViewStore } from '@/stores/view';
 import { useInputStore } from '@/stores/input';
+import Difficulty from '../components/Difficulty.vue';
 
 const view = useViewStore();
 const input = useInputStore();
 
+const difficulties = ["easy", "normal", "hard"] satisfies typeof game.difficulty[];
+let difficultiesIndex = 0;
+
 onMounted(() => {
-  input.addEventListener("a", () => view.state = "countdown", { view: "title" });
+  // NOTE: 難易度を上下で操作
+  input.addEventListener("up", upDifficulty, { view: "title" });
+  input.addEventListener("down", downDifficulty, { view: "title" });
+  input.addEventListener("a", startGame, { view: "title" });
 });
 const game = useGameStore();
+
+function upDifficulty() {
+  difficultiesIndex++;
+  if (difficultiesIndex > difficulties.length - 1) difficultiesIndex = 0;
+  game.difficulty = difficulties[difficultiesIndex];
+}
+function downDifficulty() {
+  difficultiesIndex--;
+  if (difficultiesIndex < 0) difficultiesIndex = difficulties.length - 1;
+  game.difficulty = difficulties[difficultiesIndex];
+}
+function startGame() {
+  view.state = "countdown";
+}
 </script>
 
 <template>
@@ -21,12 +42,29 @@ const game = useGameStore();
       <img :class="$style.icon" src="/icon.svg" alt="logo" />
       <h1 :class="$style.title">Languessr</h1>
     </div>
-    <div>
-      <Button :class="$style.easy" @click="game.difficulty = 'easy'">かんたん</Button>
-      <Button :class="$style.normal" @click="game.difficulty = 'normal'">ふつう</Button>
-      <Button :class="$style.hard" @click="game.difficulty = 'hard'">むずかしい</Button>
+    <div :class="$style.difficulties">
+      <Button @click="upDifficulty">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z"/></svg>
+      </Button>
+      <Transition>
+        <Difficulty v-if="game.difficulty === 'easy'" name="かんたん" color="#293a47">
+          <li>ヒントが提供されます</li>
+          <li>間違えた際のポイントの減点が少なくなります</li>
+        </Difficulty>
+        <Difficulty v-else-if="game.difficulty === 'normal'" name="ふつう" color="#514418">
+          <li>ヒントが提供されます</li>
+          <li>間違えた際にポイントが減点されます</li>
+        </Difficulty>
+        <Difficulty v-else-if="game.difficulty === 'hard'" name="むずかしい" color="#2f0929">
+          <li>ヒントは提供されません</li>
+          <li>間違えた際にポイントが減点されます</li>
+        </Difficulty>
+      </Transition>
+      <Button @click="downDifficulty">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
+      </Button>
     </div>
-    <Button :class="$style.start" @click="view.state = 'countdown'">決定キーでスタート</Button>
+    <Button :class="$style.start" @click="startGame">決定キーでスタート</Button>
   </BaseView>
 </template>
 
@@ -37,6 +75,7 @@ const game = useGameStore();
   align-items: center;
   justify-content: center;
 
+  padding-block: 2em;
   font-size: 1.2em;
 
   .titleGroup {
@@ -56,8 +95,31 @@ const game = useGameStore();
     }
   }
 
+  .difficulties {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1em;
+
+    padding-block: 2em;
+
+    flex: 1;
+
+    position: relative;
+
+    button {
+      width: 10em;
+      padding-block: 0px;
+
+      svg {
+        width: 2em;
+        height: 2em;
+        fill: currentColor;
+      }
+    }
+  }
   .start {
-    margin-block: 2em;
     font-size: 1em;
     padding: 1em 3em;
     border-radius: 1em;
