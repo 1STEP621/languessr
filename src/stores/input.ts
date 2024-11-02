21f7d1c5874d77014a077fc9db862e4d04b701f0
結葉みれい <me@mirrei.dev>
2024-11-02 14:10:52 +0900

    chore: デバッグログの修正

--- src/stores/input.ts
+++ src/stores/input.ts
@@ -46,9 +46,8 @@ export const useInputStore = defineStore("input", () => {
         const button = gamepad.buttons[i];
         const id = `${gamepad.id} / ${i}`;
 
-        if (button.value !== 0) console.log(`[${Math.floor(performance.now() / 1000)}sec] Pressed button ${i} with value ${button.value} on ${id}`);
-
-        if (button.touched) {
+        if (button.pressed) {
+          console.log(`[${Math.floor(performance.now() / 1000)}sec] Pressed button ${i} with (value ${button.value}, touched ${button.touched}) on ${id}`);
           if (!pressedButtons.has(id)) {
             press(id);
             pressedButtons.add(id);
